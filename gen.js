d2a8ce9f8609bc0d3ed9bd791a2e9828bb8d5c66
kimmywork <kimmywork@users.noreply.github.com>
2022-10-08 16:13:07 +0000

    deploy: 9ba7c9cd84d3b7a0c0f28d010b601cef6c665ee1

--- gen.js
+++
@@ -1,9 +0,0 @@
-
-const ejs = require("ejs")
-const fs = require("fs")
-
-const config = require("./config")
-
-ejs.renderFile("index.ejs.html", config, (err, result) => {
-    fs.writeFileSync("index.html", result)
-})
