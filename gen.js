9ba7c9cd84d3b7a0c0f28d010b601cef6c665ee1
kimmy <dev.kimmy.leo@gmail.com>
2022-10-09 00:12:27 +0800

    update workflow

--- gen.js
+++ gen.js
@@ -4,6 +4,8 @@ const fs = require("fs")
 
 const config = require("./config")
 
+
 ejs.renderFile("index.ejs.html", config, (err, result) => {
-    fs.writeFileSync("index.html", result)
+    fs.mkdirSync("public")
+    fs.writeFileSync("public/index.html", result)
 })
